345903e03c159c092cfc334a8abb3a7b8f1cc3af
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-09-15 18:08:27 -0300

    a few more shapes and a way to reuse some paths (?)

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let canvas, ctx;
 
+let shapes = [
+  [
+    { x: 0, y: 0 },
+    { x: 0, y: 1 },
+    { x: 1, y: 1 },
+    { x: 1, y: 0 },
+  ],
+];
+
 function init() {
   canvas = document.getElementById("cover-canvas");
   ctx = canvas.getContext("2d");
@@ -10,31 +19,59 @@ function init() {
     cols = width / s,
     rows = height / s;
 
-  for (let x = 0; x < cols; x++) {
-    for (let y = 0; y < rows; y++) {
-      let color = (x + y) % 2 == 0 ? "red" : "green";
-      drawShape(x, y, s, s, color);
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      let i = x + y * cols;
+      let color = i % 2 == 0 ? "red" : "green";
+      let iShape = i % 3;
+      drawShape(x, y, s, color, "white", iShape);
     }
   }
+}
+
+function processShape(shape, ix, iy, s) {
+  let initX = ix * s,
+    initY = iy * s;
+  ctx.moveTo(initX, initY);
+  for (let i = 1; i < shape.length; i++) {
+    let x = shape[i].x * s + initX,
+      y = shape[i].y * s + initY;
+    ctx.lineTo(x, y);
+  }
+  ctx.closePath();
 }
 
 function drawShape(
   ix,
   iy,
-  width,
-  height,
+  size,
   color = "black",
   bgcolor = "white",
   shapeIndex = 0
 ) {
-  let x = ix * width,
-    y = iy * height;
+  let x = ix * size,
+    y = iy * size,
+    cx = x + size / 2,
+    cy = y + size / 2;
 
   ctx.fillStyle = color;
   switch (shapeIndex) {
-    // square
-    case 0:
-      ctx.fillRect(x, y, width, height);
+    case 0: // square
+      ctx.beginPath();
+      processShape(shapes[0], ix, iy, size); // square
+      ctx.fill();
+      break;
+    case 1: // square with cut out circle
+      ctx.beginPath();
+      processShape(shapes[0], ix, iy, size); // square
+      ctx.arc(cx, cy, size * 0.5 * 0.5, 0, Math.PI * 2, false);
+      ctx.fill();
+      break;
+    case 2: // donut with 1/2 radius hole
+      ctx.beginPath();
+      ctx.arc(cx, cy, size * 0.5, 0, Math.PI * 2, false);
+      ctx.arc(cx, cy, size * 0.5 * 0.5, 0, Math.PI * 2, false);
+      ctx.fill("evenodd");
       break;
   }
 }
