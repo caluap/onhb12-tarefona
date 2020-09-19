5f26f59c156a5c1c1a788bad1eb848551399a8f3
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-09-19 11:04:44 -0300

    Update page-animation.js

--- scripts/page-animation.js
+++ scripts/page-animation.js
@@ -5,58 +5,44 @@ for (let i = 0; i < pages.length; i++) {
   pages[i].addEventListener(
     "click",
     function () {
-      let next = null,
-        nextPlusOne = null;
-      if (i + 1 < pages.length) {
-        next = pages[i + 1];
-      }
-      if (i + 2 < pages.length) {
-        nextPlusOne = pages[i + 2];
-      }
-
       if (i % 2 == 0) {
-        advancePage(pages[i], next, nextPlusOne);
+        advancePage(pages, i);
       } else {
-        let previous = null,
-          previousMinusOne = null;
-        if (i - 1 > 0) {
-          previous = pages[i - 1];
-        }
-        if (i - 2 > 0) {
-          previous = pages[i - 2];
-        }
-
-        retreatPage(pages[i], previous, previousMinusOne, next, nextPlusOne);
+        retreatPage(pages, i);
       }
     },
     false
   );
 }
 
-function advancePage(current, next, nextPlustOne) {
-  console.log(">>");
+function advancePage(pages, i) {
   anime({
-    targets: current,
+    targets: pages[i],
     rotateY: [0, -90],
     duration: 1500,
     easing: "easeInSine",
     begin: function (anim) {
-      current.style.zIndex = "100";
-      if (nextPlustOne) {
-        nextPlustOne.style.display = "block";
+      pages[i].style.zIndex = "100";
+      if (i + 2 < pages.length) {
+        pages[i + 2].style.display = "block";
       }
     },
     complete: function (anim) {
-      current.style.display = "none";
-      current.style.zIndex = "1";
-      if (next) {
+      pages[i].style.display = "none";
+      pages[i].style.zIndex = "1";
+      if (i + 1 < pages.length) {
         anime({
-          targets: next,
+          targets: pages[i + 1],
           rotateY: [90, 0],
           duration: 1500,
           easing: "easeOutSine",
           begin: function (anim) {
-            next.style.display = "block";
+            pages[i + 1].style.display = "block";
+          },
+          complete: function (anim) {
+            if (i - 1 >= 0) {
+              pages[i - 1].style.display = "none";
+            }
           },
         });
       }
@@ -64,33 +50,34 @@ function advancePage(current, next, nextPlustOne) {
   });
 }
 
-function retreatPage(current, previous, previousMinusOne, next, nextPlusOne) {
-  console.log("<<");
+function retreatPage(pages, i) {
   anime({
-    targets: current,
+    targets: pages[i],
     rotateY: [0, 90],
     duration: 1500,
     easing: "easeInSine",
     begin: function (anim) {
-      current.style.zIndex = "100";
-      if (previousMinusOne) {
-        previousMinusOne.style.display = "block";
+      pages[i].style.zIndex = "100";
+      if (i - 2 >= 0) {
+        pages[i - 2].style.display = "block";
       }
     },
     complete: function (anim) {
-      current.style.display = "none";
-      current.style.zIndex = "1";
-      if (next) {
-        next.style.diary = "none";
-      }
-      if (previous) {
-        console.log(previous);
-        previous.style.display = "block";
+      pages[i].style.display = "none";
+      pages[i].style.zIndex = "1";
+      if (i - 1 >= 0) {
+        pages[i - 1].style.display = "block";
+        pages[i - 1].style.display = "100";
         anime({
-          targets: previous,
+          targets: pages[i - 1],
           rotateY: [-90, 0],
           duration: 1500,
           easing: "easeOutSine",
+          complete: function (anim) {
+            if (i + 1 < pages.length) {
+              pages[i + 1].style.display = "none";
+            }
+          },
         });
       }
     },
