a909f691f539a2882c7a3f0c43578861a8b0e024
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-09-19 11:13:40 -0300

    one animation at a time

--- scripts/page-animation.js
+++ scripts/page-animation.js
@@ -1,6 +1,9 @@
 let diary = document.getElementById("diary");
 let pages = document.querySelectorAll(".page");
 
+let animating = false;
+let animDuration = 2000;
+
 for (let i = 0; i < pages.length; i++) {
   pages[i].addEventListener(
     "click",
@@ -16,70 +19,78 @@ for (let i = 0; i < pages.length; i++) {
 }
 
 function advancePage(pages, i) {
-  anime({
-    targets: pages[i],
-    rotateY: [0, -90],
-    duration: 1500,
-    easing: "easeInSine",
-    begin: function (anim) {
-      pages[i].style.zIndex = "100";
-      if (i + 2 < pages.length) {
-        pages[i + 2].style.display = "block";
-      }
-    },
-    complete: function (anim) {
-      pages[i].style.display = "none";
-      pages[i].style.zIndex = "1";
-      if (i + 1 < pages.length) {
-        anime({
-          targets: pages[i + 1],
-          rotateY: [90, 0],
-          duration: 1500,
-          easing: "easeOutSine",
-          begin: function (anim) {
-            pages[i + 1].style.display = "block";
-          },
-          complete: function (anim) {
-            if (i - 1 >= 0) {
-              pages[i - 1].style.display = "none";
-            }
-          },
-        });
-      }
-    },
-  });
+  if (!animating) {
+    animating = true;
+    anime({
+      targets: pages[i],
+      rotateY: [0, -90],
+      duration: animDuration / 2,
+      easing: "easeInQuad",
+      begin: function (anim) {
+        pages[i].style.zIndex = "100";
+        if (i + 2 < pages.length) {
+          pages[i + 2].style.display = "block";
+        }
+      },
+      complete: function (anim) {
+        pages[i].style.display = "none";
+        pages[i].style.zIndex = "1";
+        if (i + 1 < pages.length) {
+          anime({
+            targets: pages[i + 1],
+            rotateY: [90, 0],
+            duration: animDuration / 2,
+            easing: "easeOutQuad",
+            begin: function (anim) {
+              pages[i + 1].style.display = "block";
+            },
+            complete: function (anim) {
+              if (i - 1 >= 0) {
+                pages[i - 1].style.display = "none";
+              }
+              animating = false;
+            },
+          });
+        }
+      },
+    });
+  }
 }
 
 function retreatPage(pages, i) {
-  anime({
-    targets: pages[i],
-    rotateY: [0, 90],
-    duration: 1500,
-    easing: "easeInSine",
-    begin: function (anim) {
-      pages[i].style.zIndex = "100";
-      if (i - 2 >= 0) {
-        pages[i - 2].style.display = "block";
-      }
-    },
-    complete: function (anim) {
-      pages[i].style.display = "none";
-      pages[i].style.zIndex = "1";
-      if (i - 1 >= 0) {
-        pages[i - 1].style.display = "block";
-        pages[i - 1].style.display = "100";
-        anime({
-          targets: pages[i - 1],
-          rotateY: [-90, 0],
-          duration: 1500,
-          easing: "easeOutSine",
-          complete: function (anim) {
-            if (i + 1 < pages.length) {
-              pages[i + 1].style.display = "none";
-            }
-          },
-        });
-      }
-    },
-  });
+  if (!animating) {
+    animating = true;
+    anime({
+      targets: pages[i],
+      rotateY: [0, 90],
+      duration: animDuration / 2,
+      easing: "easeInQuad",
+      begin: function (anim) {
+        pages[i].style.zIndex = "100";
+        if (i - 2 >= 0) {
+          pages[i - 2].style.display = "block";
+        }
+      },
+      complete: function (anim) {
+        pages[i].style.display = "none";
+        pages[i].style.zIndex = "1";
+        if (i - 1 >= 0) {
+          pages[i - 1].style.display = "block";
+          pages[i - 1].style.display = "100";
+          anime({
+            targets: pages[i - 1],
+            rotateY: [-90, 0],
+            duration: animDuration / 2,
+            easing: "easeOutQuad",
+            complete: function (anim) {
+              if (i + 1 < pages.length) {
+                pages[i + 1].style.display = "none";
+              }
+              animating = false;
+            },
+          });
+        }
+      },
+    });
+  }
 }
