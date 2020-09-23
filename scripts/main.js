008aec8a4550533975a1aa540bef0204720c10ac
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-09-23 17:31:56 -0300

    title page generated from data

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,59 @@
 let notebook;
 
-document.fonts.load('1rem "Supermarker"').then(() => {
-  let seedString = "";
-  for (let i = 0; i < rows * cols; i++) {
-    seedString += Number(randInt(0, 15)).toString(16);
+// source: https://stackoverflow.com/a/8943487/888094
+function linkify(text) {
+  if (typeof text !== "string") {
+    return;
   }
+
+  let urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+  return text.replace(urlRegex, function (url) {
+    return `<a href="${url}" target="_blank">${url}</a>`;
+  });
+}
+
+function titlePage() {
+  let title = data.title,
+    authorship,
+    themes,
+    epigraph = data.epigraph,
+    epigraphsAuthorship = data.epigraph_authorship;
+  // authors
+  authorship = `Escrito por ${data.team_member_1[0]}., ${data.team_member_2[1]}. e ${data.team_member_3[0]}., sob orientação de ${data.team_advisor[0]}.`;
+
+  // themes
+  themes = "Uma crônica sobre <br />";
+  for (let i = 0; i < data.themes.length; i++) {
+    let theme = data.themes[i].split(" ").join("&nbsp;");
+    if (i == data.themes.length - 1) {
+      // last one
+      themes += `e ${theme}.`;
+    } else {
+      themes += `${theme}, `;
+    }
+  }
+
+  let titlePage = document.getElementById("title-page");
+  titlePage.querySelector(".title").innerHTML = title;
+  titlePage.querySelector(".authorship").innerHTML = authorship;
+  titlePage.querySelector(".theme").innerHTML = themes;
+  titlePage.querySelector(".epigraph .main-text").innerHTML = epigraph;
+  titlePage.querySelector(
+    ".epigraph .epigraph-author"
+  ).innerHTML = epigraphsAuthorship;
+}
+
+function innerPages() {}
+
+document.fonts.load('1rem "Supermarker"').then(() => {
+  // let seedString = "";
+  // for (let i = 0; i < rows * cols; i++) {
+  //   seedString += Number(randInt(0, 15)).toString(16);
+  // }
   notebook = document.getElementById("notebook");
-  console.log(seedString);
-  notebook.classList.add(`variation-${seedString[0]}`);
-  coverInit(seedString, data.team_name);
+  notebook.classList.add(`variation-${data.seed[0]}`);
+  coverInit(data.seed, data.team_name);
+  titlePage();
+  innerPages();
+  notebook.classList.remove("loading");
 });
