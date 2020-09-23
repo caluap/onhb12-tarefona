9b6927dcc45862dccd23868403152300ece74583
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-09-22 23:05:44 -0300

    refactoring of pages
    instead of mainly selecting pages by their position, they are targetted explicitly with the classes cover, flyleaf* and back-cover (and when none are present we assume they are plain old writting pages)
    
    * i'm hopefully using the correct english translation of “guarda” — although i'm not sure a brochure would have “guardas” :shrug:

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ document.fonts.load('1rem "Supermarker"').then(() => {
     seedString += Number(randInt(0, 15)).toString(16);
   }
   notebook = document.getElementById("notebook");
+  console.log(seedString);
   notebook.classList.add(`variation-${seedString[0]}`);
   coverInit(seedString);
   // diary.classList.remove("loading");
